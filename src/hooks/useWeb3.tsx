97cdfea7b31ead2601f4ef4687e568c26688f0c3
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2022-02-17 14:09:59 +0100

    first commit

---
+++ src/hooks/useWeb3.tsx
@@ -0,0 +1,11 @@
+export const useWeb3 = () => {
+   const omg = () => {
+      console.log('ok this is working')
+      console.log('ok this is working')
+      console.log('ok this is working')
+   }
+
+   return {
+      omg
+   }
+}
