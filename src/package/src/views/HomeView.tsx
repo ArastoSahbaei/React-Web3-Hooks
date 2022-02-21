ddd7d73d615eaa94ede94122b874458568ed525d
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2022-02-21 16:48:53 +0100

    m

--- src/package/src/views/HomeView.tsx
+++ src/package/src/views/HomeView.tsx
@@ -1,18 +1,12 @@
-import { useWeb3 } from "../useWeb3"
-
 export const HomeView = () => {
-   const { verifyWalletExtension, connectToWallet, getWalletBalance, createPayment } = useWeb3()
 
    return (
       <div>
-         <h1>The #1 React-Hook to Access the Decentralized Web</h1>
+         <h1>React-Web3-Hooks Provides Developers Easy Access to the Decentralized Web</h1>
          <h3>Installation:</h3>
-         <h3>Github link</h3>
+         <h3>Become a Contributor? Github link</h3>
          <h3>NPM package link</h3>
-         <button onClick={() => verifyWalletExtension()}>verifyWalletExtension</button>
-         <button onClick={() => console.log(connectToWallet())}>connectToWallet</button>
-         <button onClick={() => console.log(getWalletBalance('0xf4a2f01cd178b88b7946708f4f7a053aae8cb0c1'))}>getWalletBalance</button>
-         <button onClick={() => console.log(createPayment('0xf4a2f01cd178b88b7946708f4f7a053aae8cb0c1', 0.02))}>createPayment</button>
+         <h3>Donations</h3>
       </div>
    )
 }
