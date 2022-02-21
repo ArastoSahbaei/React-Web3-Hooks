8af05d9c22f799977f933c4ec986dce037e9bb57
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2022-02-21 12:11:09 +0100

    m

--- src/package/src/views/components/CodeHighLight.tsx
+++ src/package/src/views/components/CodeHighLight.tsx
@@ -2,11 +2,15 @@ import SyntaxHighlighter from 'react-syntax-highlighter';
 import { dark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 export const CodeHighLight = () => {
-   const codeString = 'import { useWeb3 } from \'react-web3-hooks\''
+   const reactImport = 'import react from \'react\' \n'
+   const hookImport = 'import { useWeb3 } from \'react-web3-hooks\' \n\n'
+   const usage = '      <button onClick={() => alert(verifyWalletExtension)}>Call Function</button>'
+   const RAFC = `export const Example = () => {\n const { verifyWalletExtension } = useWeb3() \n return (\n    <div> \n${usage}   \n   </div> \n )}`
+
    return (
       <>
          <SyntaxHighlighter showLineNumbers={true} language="javascript" style={dark}>
-            {'import { useWeb3 } from \'react-web3-hooks\' \n\n export const Example = () => {\n const { verifyWalletExtension } = useWeb3() \n return(\n <div>)}'}
+            {reactImport + hookImport + RAFC}
          </SyntaxHighlighter>
       </>
    )
